967654073ae2319eee17280465e297ef8711262f
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-26 13:21:02 -0400

    PawnMovement 'one space forawrd' fails

---
+++ CucumberTests/PawnMovementSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class PawnMovementSteps
+    {
+        ChessBoard board = null;
+        [Given(@"that I have an empty chessboard")]
+        public void GivenThatIHaveAnEmptyChessboard()
+        {
+            board = new ChessBoard();
+        }
+
+        Pawn p = null;
+        [Given(@"I have a white pawn at (.*)(.*)")]
+        public void GivenIHaveAWhitePawnAt(char x, int y)
+        {
+            p = new Pawn("white");
+            board.placePiece(p, new Tuple<char, int>(x, y));
+        }
+        
+        [When(@"I try to move the white pawn from (.*)(.*) to (.*)(.*)")]
+        public void WhenITryToMoveTheWhitePawnFromTo(char x1, int y1, char x2, int y2)
+        {
+            board.movePiece(new Tuple<char, int>(x1, y1), new Tuple<char, int>(x2, y2));
+        }
+        
+        [Then(@"the white pawn is placed in (.*)(.*)")]
+        public void ThenTheWhitePawnIsPlacedIn(char x, int y)
+        {
+            
+        }
+    }
+}
