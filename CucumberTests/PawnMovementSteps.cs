81fd37bcfa9b05bccdbe50a61191c62a219b9604
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-29 11:25:41 -0400

    one space forward fails

--- CucumberTests/PawnMovementSteps.cs
+++ CucumberTests/PawnMovementSteps.cs
@@ -14,23 +14,23 @@ namespace CucumberTests
         }
 
         Pawn p = null;
-        [Given(@"I have a white pawn at (.*)(.*)")]
+        [Given(@"I have a white pawn at (.)(.)")]
         public void GivenIHaveAWhitePawnAt(char x, int y)
         {
             p = new Pawn("white");
             board.placePiece(p, new Tuple<char, int>(x, y));
         }
         
-        [When(@"I try to move the white pawn from (.*)(.*) to (.*)(.*)")]
+        [When(@"I try to move the white pawn from (.)(.) to (.)(.)")]
         public void WhenITryToMoveTheWhitePawnFromTo(char x1, int y1, char x2, int y2)
         {
             board.movePiece(new Tuple<char, int>(x1, y1), new Tuple<char, int>(x2, y2));
         }
         
-        [Then(@"the white pawn is placed in (.*)(.*)")]
+        [Then(@"the white pawn is placed in (.)(.)")]
         public void ThenTheWhitePawnIsPlacedIn(char x, int y)
         {
-            
+            Assert.AreEqual(new Tuple<char, int>(x, y), board.getLocation(p));
         }
     }
 }
