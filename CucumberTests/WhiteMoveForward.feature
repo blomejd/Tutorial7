5c1de1502f573250cf178b16b5932e4e41683bc1
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-29 13:00:19 -0400

    WhiteMoveForward fails

---
+++ CucumberTests/WhiteMoveForward.feature
@@ -0,0 +1,10 @@
+﻿Feature: WhiteMoveForward
+	As a player,
+	I cannot move my pawn backward
+
+@movement @forward
+Scenario: WhiteForward
+	Given that I have an empty chessboard
+	And I have a white pawn at a2
+	When I try to move the white pawn from a2 to b2
+	Then the white pawn will not move
---
+++ CucumberTests/WhiteMoveForwardSteps.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using Tutorial7;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class WhiteMoveForwardSteps
+    {
+        [Then(@"the white pawn will not move")]
+        public void ThenTheWhitePawnWillNotMove()
+        {
+            ChessBoard b = new ChessBoard();
+            Pawn p = new Pawn("white");
+            Tuple<char, int> init = new Tuple<char, int>('b', 2);
+            b.placePiece(p, init);
+            b.movePiece(init, new Tuple<char, int>('b', 5));
+            Assert.IsFalse(p.hasMoved());
+            b.movePiece(init, new Tuple<char, int>('a', 2));
+            Assert.IsFalse(p.hasMoved());
+            b.movePiece(init, new Tuple<char, int>('a', 4));
+            Assert.IsFalse(p.hasMoved());
+            b.movePiece(init, new Tuple<char, int>('b', 1));
+            Assert.IsFalse(p.hasMoved());
+            b.movePiece(init, new Tuple<char, int>('c', 2));
+            Assert.IsFalse(p.hasMoved());
+            b.movePiece(init, new Tuple<char, int>('c', 4));
+            Assert.IsFalse(p.hasMoved());
+
+        }
+    }
+}
