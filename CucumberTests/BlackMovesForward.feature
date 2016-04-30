cda8ff795255c3e97d2b75312063bc714de6364d
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-29 20:59:12 -0400

    BlackForward fails

---
+++ CucumberTests/BlackMovesForward.feature
@@ -0,0 +1,10 @@
+﻿Feature: BlackMovesForward
+	As a player,
+	I cannot move a black pawn backward
+
+@movement @forward
+Scenario: BlackForward
+	Given that I have an empty chessboard
+	And I have a black pawn at a7
+	When I try to move the black pawn from a7 to a8
+	Then the black pawn will not move
---
+++ CucumberTests/BlackMovesForwardSteps.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using Tutorial7;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class BlackMovesForwardSteps
+    {
+        ChessBoard board;
+        Pawn p;
+        [When(@"I try to move the black pawn from (.)(.) to (.)(.)")]
+        public void WhenITryToMoveTheBlackPawnFromTo(char x1, int y1, char x2, int y2)
+        {
+            Tuple<char, int> init = new Tuple<char, int>('a', 7);
+            board = new ChessBoard();
+            p = new Pawn("black");
+            board.placePiece(p, init);
+            board.movePiece(init, new Tuple<char, int>('a', 8));
+        }
+        
+        [Then(@"the black pawn will not move")]
+        public void ThenTheBlackPawnWillNotMove()
+        {
+            Assert.IsFalse(p.hasMoved());
+        }
+    }
+}
