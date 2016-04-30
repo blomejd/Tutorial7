eb0d8938e44a8d627245814fcea524886459439c
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-29 22:03:11 -0400

    OnlyCaptureDiagonallyFails

---
+++ CucumberTests/OnlyCaptureDiagonally.feature
@@ -0,0 +1,11 @@
+﻿Feature: OnlyCaptureDiagonally
+	As a player,
+	I can only move a pawn diagonally when a piece is being captured
+
+@capture
+Scenario: Only Capture Diagonally
+	Given that I have an empty chessboard
+	And I have a white pawn at a2
+	And A black pawn is not at b3
+	When I try to move the white pawn from a2 to b3
+	Then the white pawn is placed in a2
---
+++ CucumberTests/OnlyCaptureDiagonallySteps.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using Tutorial7;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class OnlyCaptureDiagonallySteps
+    {
+        PawnMovementSteps moveSteps;
+        public OnlyCaptureDiagonallySteps(PawnMovementSteps p)
+        {
+            moveSteps = p;
+        }
+        [Given(@"A black pawn is not at (.)(.)")]
+        public void GivenABlackPawnIsNotAtB(char x, int y)
+        {
+            Pawn p = moveSteps.board.GetPieceAt(x, y);
+            if (p != null)
+            {
+                Assert.AreEqual("black", p.color);
+            }
+        }
+    }
+}
