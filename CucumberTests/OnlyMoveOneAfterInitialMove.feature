3971d3a8c23283ae56c650308b2d67255b2f4dc5
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-29 21:30:36 -0400

    OnlyMoveOneAfterInitial fails

---
+++ CucumberTests/OnlyMoveOneAfterInitialMove.feature
@@ -0,0 +1,11 @@
+﻿Feature: OnlyMoveOneAfterInitialMove
+	As a player,
+	Once I have moved a pawn, it can only move one space at a time
+
+@movement
+Scenario: OneSpacePerMoveAfterInitial
+	Given that I have an empty chessboard
+	And I have a white pawn at a2
+	And I move the white pawn from a2 to a4
+	When I try to move the white pawn from a4 to a6
+	Then the white pawn is placed in a4
---
+++ CucumberTests/OnlyMoveOneAfterInitialMoveSteps.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using Tutorial7;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class OnlyMoveOneAfterInitialMoveSteps
+    {
+        [Given(@"I move the white pawn from (.)(.) to (.)(.)")]
+        public void GivenIMoveTheWhitePawnFromAToA(char x1, int y1, char x2, int y2)
+        {
+            ChessBoard b = new ChessBoard();
+            Pawn p = new Pawn("white");
+            Tuple<char, int> init = new Tuple<char, int>('a', 2);
+            Tuple<char, int> next = new Tuple<char, int>('a', 4);
+            Tuple<char, int> last = new Tuple<char, int>('a', 6);
+
+            b.placePiece(p, init);
+            b.movePiece(init, next);
+            b.movePiece(next, last);
+
+            Assert.AreEqual(next, b.getLocation(p));
+        }
+    }
+}
