8234e16a05005387f8af3eea0388f02e1a933258
Joshua Blome <blomejd@rose-hulman.edu>
2016-04-29 22:33:19 -0400

    Capture in passing fails

---
+++ CucumberTests/CaptureInPassing.feature
@@ -0,0 +1,13 @@
+﻿Feature: CaptureInPassing
+	As a player,
+	I can capture a pawn that has just moved two spaces by capturing into the space it skipped
+
+@mytag
+Scenario: Capture black pawn in passing
+	Given that I have an empty chessboard
+	And I have a white pawn at a5
+	And I have a black pawn at b7
+	And I move the black pawn from b7 to b5
+	When I try to move the white pawn from a5 to b6
+	Then the white pawn is placed in b6
+	And the black pawn is captured
---
+++ CucumberTests/CaptureInPassingSteps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Tutorial7;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class CaptureInPassingSteps
+    {
+        PawnMovementSteps moveSteps;
+        public CaptureInPassingSteps(PawnMovementSteps p)
+        {
+            moveSteps = p;
+        }
+
+        [Given(@"I move the black pawn from (.)(.) to (.)(.)")]
+        public void GivenIMoveTheBlackPawnFromTo(char x1, int y1, char x2, int y2)
+        {
+            moveSteps.board.movePiece(new Tuple<char, int>(x1, y1), new Tuple<char, int>(x2, y2));
+        }
+    }
+}
